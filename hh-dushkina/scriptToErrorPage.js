50be14c03dfa195d5063ee2b96c2c85db1627b76
Ekaterina Dushkina <dushkina.katya24@mail.ru>
2025-11-10 20:51:34 +0300

    remove copy404

--- hh-dushkina/scriptToErrorPage.js
+++ hh-dushkina/scriptToErrorPage.js
@@ -1,18 +1,18 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const distDir = path.resolve(__dirname, '..', 'dist');
-const indexPath = path.join(distDir, 'index.html');
-const targetPath = path.join(distDir, '404.html');
+// const distDir = path.resolve(__dirname, '..', 'dist');
+// const indexPath = path.join(distDir, 'index.html');
+// const targetPath = path.join(distDir, '404.html');
 
-if (!fs.existsSync(indexPath)) {
-  console.error('Не найден файл:', indexPath);
-  process.exit(1);
-}
+// if (!fs.existsSync(indexPath)) {
+//   console.error('Не найден файл:', indexPath);
+//   process.exit(1);
+// }
 
-fs.copyFileSync(indexPath, targetPath);
-console.log('copied', indexPath, '->', targetPath);
+// fs.copyFileSync(indexPath, targetPath);
+// console.log('copied', indexPath, '->', targetPath);
